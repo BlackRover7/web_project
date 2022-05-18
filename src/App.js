8575cc33d66d8b45ae0b8697486c6541798fd7a0
BlackRover7 <71547659+BlackRover7@users.noreply.github.com>
2022-05-18 22:36:27 +0400

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
+import React from "react";
+import { PageNotFound, News, New, Bookmarks, About } from "./index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <HashRouter>
+      <header className="mainMenu">
+        <nav>
+          <Link className="menuLink" to="/news"> Новости</Link>
+          <Link className="menuLink" to="/about"> О приложении</Link>
+          <Link className="menuLink" to="/bookmarks"> Закладки</Link>
+        </nav>
       </header>
-    </div>
-  );
+
+      <main>
+        <Routes>
+          <Route path="*" element={<PageNotFound />}>
+          </Route>
+          <Route exact path="/ppireact" element={<News />}>
+          </Route>
+          <Route path="/about" element={<About />}>
+          </Route>
+          <Route path="/news" element={<News />}>
+          </Route>
+          <Route path="/news/:id" element={<New />}>
+          </Route>
+          <Route path="/:" element={<News />}>
+          </Route>
+          <Route exact path="/" element={<News />}>
+          </Route>
+          <Route path="/bookmarks" element={<Bookmarks />}>
+          </Route>
+        </Routes>
+      </main>
+    </HashRouter>)
 }
 
 export default App;
